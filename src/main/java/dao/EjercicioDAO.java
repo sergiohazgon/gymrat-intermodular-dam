d9d0a43d0e480990b45303088eee01e7176eaf2c
Sergio <sergiohaz18@gmail.com>
2026-04-28 19:08:34 +0200

    Remplazo de impor sql.* por las sql single class que voy a necesitar
    Me importa demasiadas clases que no estoy utilizando

--- src/main/java/dao/EjercicioDAO.java
+++ src/main/java/dao/EjercicioDAO.java
@@ -3,7 +3,10 @@ package dao;
 import database.DBconnection;
 import model.Ejercicio;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class EjercicioDAO {
